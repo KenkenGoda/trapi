3be3a78a7b1788ef0160ced7c6aa9440e0f34648
Shohei Goda <shohei_goda@fringe81.com>
2021-01-16 23:51:51 +0900

    Fix encode functions

--- trapi/encode.py
+++ trapi/encode.py
@@ -1,55 +1,46 @@
 from collections import Counter
+
+import numpy as np
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 
 
-def label_encode(df, column, append_column=False, return_encoder=False):
+def label_encode(values, return_encoder=False):
     # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder
     encoder = LabelEncoder()
-    encoded = encoder.fit_transform(df[column].values)
-    if append_column:
-        column += "_encoded"
-    df[column] = encoded
+    encoded_values = encoder.fit_transform(values)
     if return_encoder:
-        return df, encoder
+        return encoded_values, encoder
     else:
-        return df
+        return encoded_values
 
 
-def ordinal_encode(df, columns, append_columns=False, return_encoder=False):
+def ordinal_encode(values, return_encoder=False):
     # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OrdinalEncoder
     encoder = OrdinalEncoder()
-    encoded = encoder.fit_transform(df[columns].values)
-    if append_columns:
-        columns = [col + "_encoded" for col in columns]
-    df[columns] = encoded
+    encoded_values = encoder.fit_transform(values)
     if return_encoder:
-        return df, encoder
+        return encoded_values, encoder
     else:
-        return df
+        return encoded_values
 
 
-def count_encode(df, column, append_column=False, return_encoder=False):
+def count_encode(values, return_encoder=False):
     # https://docs.python.org/ja/3/library/collections.html?highlight=collections%20counter#collections.Counter
-    counter = Counter(df[column])
+    counter = Counter(values)
     count_dict = dict(counter.most_common())
-    encoded = df[column].map(count_dict).values
-    if append_column:
-        column += "_count"
-    df[column] = encoded
+    encoded_values = np.array([count_dict[value] for value in values])
     if return_encoder:
-        return df, count_dict
+        return encoded_values, count_dict
     else:
-        return df
+        return encoded_values
 
 
-def hash_encode(df, column, n_features=10, return_encoder=False):
+def hash_encode(values, n_features=10, return_encoder=False):
     # https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.FeatureHasher
     encoder = FeatureHasher(n_features=n_features)
-    encoded = encoder.fit_transform(df[column].values).toarray()
-    columns = [column + f"_{i}" for i in range(n_features)]
-    df[columns] = encoded
+    encoded_values = encoder.fit_transform(values).toarray()
     if return_encoder:
-        return df, encoder
+        return encoded_values, encoder
     else:
-        return df
+        return encoded_values
